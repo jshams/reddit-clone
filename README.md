db8ad80ca7c3daa48b2de3a01f1c65d9a46db9cf
Jake Shams <jshams62@gmail.com>
2019-02-14 05:01:42 -0800

    Users can submit and save posts

---
+++ README.md
@@ -0,0 +1,15 @@
+1. Create a post
+[ ] Make a /posts/new route (/posts/new) and template (posts-new.handlebars)
+[ ] Add form to posts-new template
+[ ] Make create posts route and check that form data is sending to new route
+[ ] Add Post model with mongoose
+[ ] Confirm posts are saving to database
+
+2. Show all posts
+3. Show one post
+4. Comment on posts
+5. Create subreddits
+6. Sign up and Login
+7. Associate posts and comments with their author
+8. Make comments on comments
+9. Vote a post up or down
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express()
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
+require('./controllers/posts.js')(app);
+// Set db
+require('./data/reddit-db');
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// Add after body parser initialization!
+app.use(expressValidator());
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   res.render('home', {msg: 'reddit.js'});
+})
+
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new');
 })
 
 
