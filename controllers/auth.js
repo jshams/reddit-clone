ff925bed72baa4e4d96babd8bbfe3e36ef5db976
Jake Shams <jshams62@gmail.com>
2019-03-03 14:43:47 -0800

    Users can log out

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,6 +6,7 @@ module.exports = app => {
   app.get("/sign-up", (req, res) => {
     res.render("sign-up");
   });
+  
   // SIGN UP POST
   app.post("/sign-up", (req, res) => {
     // Create User and JWT
@@ -22,5 +23,11 @@ module.exports = app => {
         console.log(err.message);
         return res.status(400).send({ err: err });
       });
+  });
+
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    res.redirect('/');
   });
 };
